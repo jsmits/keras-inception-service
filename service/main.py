b35c2db4b12261ab0edb15400f88135a7786a384
Sander Smits <sander.smits@nelen-schuurmans.nl>
2017-05-07 14:08:40 +0200

    PEP8.

--- service/main.py
+++ service/main.py
@@ -1,23 +1,28 @@
-from flask import Flask, current_app, request, jsonify
-import io
-import model
 import base64
-import logging
+import io
+
+from flask import Flask
+from flask import current_app
+from flask import jsonify
+from flask import request
+
+import model
 
 app = Flask(__name__)
+
+
 @app.route('/', methods=['POST'])
 def predict():
-    data = {}
     try:
         data = request.get_json()['data']
     except Exception:
         return jsonify(status_code='400', msg='Bad Request'), 400
 
     data = base64.b64decode(data)
-
     image = io.BytesIO(data)
     predictions = model.predict(image)
     current_app.logger.info('Predictions: %s', predictions)
+
     return jsonify(predictions=predictions)
 
 
--- service/model.py
+++ service/model.py
@@ -1,25 +1,41 @@
 import keras
+
+from keras.applications.inception_v3 import decode_predictions
+from keras.applications.inception_v3 import preprocess_input
 from keras.preprocessing import image
-from keras.applications.inception_v3 import preprocess_input, decode_predictions
+
 import numpy as np
+
 import tensorflow as tf
 
-model = keras.applications.inception_v3.InceptionV3(include_top=True, weights='imagenet', input_tensor=None, input_shape=None)
+
+model = keras.applications.inception_v3.InceptionV3(
+    include_top=True, weights='imagenet', input_tensor=None, input_shape=None)
 graph = tf.get_default_graph()
 
 
 def predict(image_file):
+    """
+    Predict the top 3 categories for the given image file.
+
+    :param image_file - path to the image file
+
+    :return - predictions dictionary containing label, description and
+        probability
+
+    """
     img = image.load_img(image_file, target_size=(299, 299))
     x = image.img_to_array(img)
-    x = np.expand_dims(x,axis=0)
+    x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
 
     global graph
     with graph.as_default():
-        preds = model.predict(x)
+        results = model.predict(x)
 
-    top3 = decode_predictions(preds,top=3)[0]
-
-    predictions = [{'label': label, 'description': description, 'probability': probability * 100.0}
-                    for label,description, probability in top3]
-    return predictions
+    top3 = decode_predictions(results, top=3)[0]
+    return [
+        {'label': label, 'description': description,
+         'probability': probability * 100.0}
+        for label, description, probability in top3
+    ]
